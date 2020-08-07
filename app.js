6e0c131c77e47502e6e8dc0b1f87912efe551fb4
Levin Batallones <67178203+vbatallones@users.noreply.github.com>
2020-08-07 12:25:03 -0700

    [app.js] Add db methods to access data

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
-const db = require('./models');
+//create
+ const db = require('./models');
 db.user.create({
     firstName: 'Levin',
     lastName: 'Batallones',
     age: 25,
     email: 'levinbatallones@yahoo.com'
+}).then(data => {
+    console.log(data)
+});
+
+//searching by id.
+db.user.findOne({
+    where: {id: 1}
+}).then(user => {
+    console.log(user.dataValues)
+})
+//searching by name
+db.user.findOne({
+    where: {firstName: 'Levin'}
+}).then(user => {
+    console.log(user)
+})
+//searching by email
+db.user.findOne({
+    where: {email: 'levinbatallones@yahoo.com'}
+}).then(user => {
+    console.log(user)
+})
+db.user.findOrCreate({
+    where: {
+        firstName: 'Kevin',
+        lastName: 'Johnson'
+    },
+    defaults: {age: 40, email: 'kv@email.com'}
+}).then(([user, created]) => {
+    console.log(`This was created on ${created}`)
+    console.log(user.dataValues)
+})
+
+let queryOne = {
+    where: {
+        firstName: 'John',
+        lastName: 'Smith'
+    },
+    defaults: {age: 25, email: 'jsjs@gmail.com'}
+};
+
+db.user.findOrCreate (queryOne).then(([user, created]) => {
+    console.log(created);
+    console.log(user.dataValues)
+})
+
+db.user.findAll().then(user => {
+    user.forEach(u => {
+        
+        console.log(u.firstName+ " " +u.lastName)
+    });
 })
